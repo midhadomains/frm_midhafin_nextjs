26069d490cb646680f07fc8ced06a090b62a09a7
sujalsahu620 <sahusujal620@gmail.com>
2024-04-16 17:37:22 +0530

    done

--- app/part-1/page.jsx
+++ app/part-1/page.jsx
@@ -33,22 +33,23 @@ export default function page() {
       data: "The FRM Part 1 exam is valid for a period of 4 years. Candidates must pass the FRM Part 2 exam before this period is over to earn FRM designation, otherwise, they will have to reappear for the FRM Part 1 exam.",
     },
   ];
-
   return (
     <div className="bg-white">
       <Navbar />
       <div className="text-center bg-gradient-to-b from-[#ffe4a4a0] from-[1%] to-[100%] p-4 max-h-[300px]">
         {/* <div className=" h-[50px] max-w-[700px] mx-auto  "></div> */}
-        <h1 className="text-[50px] font-semibold text-[#6A1C1A] mt-[70px]">
+        <h1 className="text-[24px] md:text-[30px] lg:text-[40px] xl:text-[50px] font-semibold text-[#6A1C1A] mt-[70px]">
           <span className="text-[#BF4E1E]">F</span>inancial&nbsp;
           <span className="text-[#BF4E1E]">R</span>isk&nbsp;
           <span className="text-[#BF4E1E]">M</span>anagement
         </h1>
-        <h1 className="text-[#6A1C1A] text-[70px] font-bold">FRM Part - 1</h1>
+        <h1 className="text-[#6A1C1A] text-[40px] md:text-[50px] lg:text-[60px] xl:text-[70px]  font-bold">FRM Part - 1</h1>
       </div>
       <Courses />
       {/* Eligibility and pre Requisites */}
-      <div className="flex h-[420px] justify-center my-[70px]">
+
+      {/* Large screem */}
+      <div className="hidden  lg:flex h-[420px] justify-center my-[70px]">
         <div className="bg-[#FFF4DA] min-w-[300px] flex justify-end relative w-[50%]">
           <Image src='/part-1/Part-1left.svg' width={350} height={450} alt="image" className="absolute -right-[60px]" />
         </div>
@@ -59,8 +60,20 @@ export default function page() {
           </div>
         </div>
       </div>
+      {/* Small screen */}
+      <div className="w-[100%] lg:hidden mt-[15%] sm:mt-[25%] ">
+        <div className="bg-[#FFF4DA] h-[150px] mt-auto relative ">
+          <Image src='/part-1/Part-1left.svg' width={170} height={100} alt="image" className="mx-auto sm:w-[30%] bottom-0 fixed " />
+        </div>
+        <div className="px-6 max-w-[700px] mx-auto">
+          <h2 className="text-[15px] md:text-[20px] lg:text-[25px] font-bold ">Eligibility and/or Pre-Requisites</h2>
+          <p>The FRM course welcomes candidates from various academic backgrounds, without any specific educational field prerequisites. There are NO ELIGLIBILITY CRITERIA for potential candidates; even those in the early stages of their undergraduate education can embark on the FRM journey by taking the Part 1 examination.</p>
+        </div>
+      </div>
       {/* About the exam */}
-      <div className="flex h-[450px] justify-center mt-[200px]">
+
+      {/* Large screem */}
+      <div className="hidden  lg:flex h-[450px] justify-center mt-[200px]">
         <div className=" flex justify-end w-[80%] mr-[100px] ">
           <div className="max-w-[900px] h-[100%] flex flex-col justify-end pl-4">
             <h1 className="text-[35px] xl:text-[40px] mb-[20px] xl:mb-[40px] font-semibold">About the Exam - <span className="text-[#BF4E1E]">FRM Part 1</span></h1>
@@ -71,6 +84,8 @@ export default function page() {
           <Image src='/part-1/part-1right.svg' width={400} height={350} alt="image" className="absolute -left-[60px] bottom-0" />
         </div>
       </div>
+      {/* Small screen */}
+
       {/* weightage module */}
       <div className="max-w-[1350px] mx-auto px-4 w-[100%] mt-[100px]">
         <h1 className="text-[20px] md:text-[30px]  lg:text-[40px] font-semibold text-left my-[50px]">The specific weightage for each module in the exam is as follows:</h1>
@@ -113,7 +128,7 @@ export default function page() {
           </li>
         </ul>
         {/* desc section */}
-        <div className="max-w-[1300px] mx-auto">
+        <div className="max-w-[1300px] mx-auto hidden md:block">
           <div className="flex justify-between  mt-[80px]">
             <div className="max-w-[1000px] pr-5">
               <h2 className="md:text-[30px] lg:text-[35px] font-bold ">Foundations of Risk Management</h2>
@@ -145,7 +160,7 @@ export default function page() {
         </div>
       </div>
       {/* Composition and Exam Structure */}
-      <div className="flex h-[520px] justify-center my-[70px] mt-[150px]">
+      <div className="hidden  lg:flex h-[520px] justify-center my-[70px] mt-[150px]">
         <div className="bg-[#FFF4DA] min-w-[300px] flex justify-end relative w-[50%]">
           <Image src='/part-1/caes.svg' width={400} height={450} alt="image" className="absolute -right-[60px] top-[40px]" />
         </div>
@@ -157,13 +172,13 @@ export default function page() {
         </div>
       </div>
       {/* study plan */}
-      <div className="max-w-[1300px] mx-auto flex justify-between ">
+      <div className="max-w-[1300px] mx-auto flex justify-center md:justify-between flex-wrap p-5">
         <div className="max-w-[680px] mt-auto">
           <h2 className="text-[30px] xl:text-[35px] mb-[30px] xl:mb-[50px]  font-semibold">Study Plan</h2>
           <p className="text-[20px] xl:text-[24px] leading-[40px] font-[400]  ">Ideally, a candidate preparing for the FRM Part 1 exam should complete the whole curriculum in 16 weeks with 30-35 hours of study every week. This study plan provides a 16-week road map with relevant chapters that should be covered every week It also provides a carefully researched chapter sequence that can be followed to ensure maximum understanding of the FRM Part 1 curriculum.</p>
         </div>
         <div className="flex-shrink-0">
-          <div className="my-8 " style={{ width: "500px", maxWidth: "500px",minWidth:"200px", }}>
+          <div className="my-8 w-[100%]" style={{ width: "100%", maxWidth: "500px",minWidth:"200px", }}>
             <div style={{ position: "relative", paddingBottom: "62.5%", height: "0", overflow: "hidden", maxWidth: "100%",  }}>
               <iframe
                 src="https://www.youtube.com/embed/6sTEUkR40uI?si=sDonMG5jq4orT3Ue"
@@ -178,31 +193,31 @@ export default function page() {
         </div>
       </div>
       {/* Preparation Tips */}
-      <div className="my-[100px] max-w-[1300px] mx-auto px-3 ">
+      <div className="my-[100px] max-w-[1300px] mx-auto px-5 ">
         <h1 className="text-[20px] md:text-[30px]  lg:text-[40px] font-bold text-center my-[10px]">Preparation Tips</h1>
         <p className="text-center  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">Preparing for FRM Part 1 involves careful consideration of several crucial aspects. The effective selection and management of these factors play a pivotal role in ensuring a robust preparation:</p>
-        <div className="grid grid-cols-2 mt-[40px]">
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+        <div className="grid md:grid-cols-2 grid-cols-1 mt-[40px] px-1 overflow-hidden">
+          <div className="min-w-[300px] max-w-[600px] w-[100%] my-3  md:m-5   ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">1. Preparation Window </h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">Allocating a preparation window of at least 5 months for FRM Part 1 is advisable.</p>
           </div>
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+          <div className="min-w-[300px] max-w-[600px] w-[100%] my-3 md:m-5   ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">2. Time Required </h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">To achieve thorough preparation, it is recommended to dedicate around 600 hours to FRM Part 1. Avoiding shortcuts is emphasized to ensure a deep understanding of risk management concepts and tools, facilitating ease in answering interview questions.</p>
           </div>
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+          <div className="min-w-[300px] max-w-[600px] w-[100%] my-3   md:m-5  ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">3. Material</h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">Choosing the right study material is paramount. MidhaFin, as one of the first GARP Authorized Prep Providers for FRM, offers comprehensive study material, including over 200 hours of content videos, 85 plus hours of question-solving videos, and 100 plus hours of revision videos.</p>
           </div>
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+          <div className="min-w-[300px] max-w-[600px] w-[100%]  my-3 md:m-5   ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">4. Questions</h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">Solving a substantial number of questions is key to preparation success. MidhaFin provides question banks with over 3000 questions, closely aligned with recent exam trends. GARP sample paper questions from the past 15 years are also included.</p>
           </div>
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+          <div className="min-w-[300px] max-w-[600px] w-[100%] my-3  md:m-5   ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">5. Periodic Tests</h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">Implementing regular quizzes is integral to the active learning philosophy at MidhaFin. Four monthly tests are conducted, focusing on the curriculum taught during the respective month. Graded tests enable students to monitor their performance consistently.</p>
           </div>
-          <div className="min-w-[300px] max-w-[600px] w-[100%]  m-5   ">
+          <div className="min-w-[300px] max-w-[600px] w-[100%]  my-3 md:m-5   ">
             <h2 className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[40px] font-[600] text-[#6A1C1A] ">6. Mock Tests</h2>
             <p className="text-left  mx-auto text-[20px] xl:text-[24px] leading-[34px] font-[400] ">MidhaFin conducts 5 mock tests towards the end of the preparation period, starting approximately a month before the exam. These full-length mock tests cover the entire syllabus and are graded, providing quartile analysis similar to the actual GARP FRM exams.</p>
           </div>
--- components/Part-1/Courses.jsx
+++ components/Part-1/Courses.jsx
@@ -67,10 +67,10 @@ export default function Courses() {
     ]
 
     return (
-        <div className="max-w-[1400px] mx-auto mt-[70px]">
-            <div className="flex justify-between px-6">
-                <h2 className="text-[40px] font-semibold">Courses Offered</h2>
-                <Link href='https://edu.midhafin.com/s/store' className="flex text-[24px] font-semibold text-[#BE4E1E] my-auto">View More Courses <FaAngleRight className="mt-2" /></Link>
+        <div className="max-w-[1400px] mx-auto mt-[30px] md:mt-[70px]">
+            <div className="flex justify-center md:justify-between px-6">
+                <h2 className=" text-[25px] md:text-[30px] lg:text-[35px] xl:text-[40px] font-semibold">Courses Offered</h2>
+                <Link href='https://edu.midhafin.com/s/store' className=" hidden md:flex text-[24px] font-semibold text-[#BE4E1E] my-auto">View More Courses <FaAngleRight className="mt-2" /></Link>
             </div>
             <div className="flex justify-around flex-wrap ">
                 {
@@ -79,6 +79,7 @@ export default function Courses() {
                     ))
                 }
             </div>
+            <Link href='https://edu.midhafin.com/s/store' className=" md:hidden flex text-[24px] font-semibold text-[#BE4E1E] my-auto justify-center">View More Courses <FaAngleRight className="mt-2" /></Link>
         </div>
     );
 }
