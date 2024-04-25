44adb8c066a09a6fe3b85332807d3d6fabbf48df
SachinSemwal007 <sachinsemwal18@gmail.com>
2024-04-25 18:57:11 +0530

    Completed Dates and Deadlines

--- app/part-1/dates-and-deadlines/page.jsx
+++ app/part-1/dates-and-deadlines/page.jsx
@@ -7,128 +7,168 @@ export default function page() {
         FRM -<span className="font-semibold"> Exam Dates</span>
       </h1>
 
-      <p className="font-semibold text-[16px] sm:text-[36px] max-w-[1350px] mx-auto px-[25px] my-9">
+      <p className="font-semibold text-[16px] sm:text-[36px] text-center  max-w-[1350px] mx-auto px-[25px] my-9">
         The exam window are provided below:
       </p>
 
       <div className="bg-[#FFEFCB] py-4">
-        <h1 className="font-bold mx-auto max-w-[1350px] text-center sm:text-left px-[25px] text-[14px] sm:text-[36px]">
+        <h1 className="font-bold mx-auto max-w-[1350px] text-center  px-[25px] text-[14px] sm:text-[36px]">
           MAY EXAMS
         </h1>
       </div>
 
-      <div className="max-w-[1350px] mx-auto px-[25px] mt-[41px]">
+      {/* <div className="max-w-[1350px] mx-auto px-[25px] mt-[41px]">
         <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-bold text-center">
           Exam Dates
         </h1>
-      </div>
+      </div> */}
 
-      
-        <div class="border-l-4 border-gray-400  ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] md:-ml-[14px] lg:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part I - May 11 - 17, 2024</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] md:-ml-[14px] lg:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part II - May 18  - 22, 2024</h1>
-        </div>   
+      {/* <div>
+        <div className="border-l-4 border-gray-400 m-9 max-w-[600px] mx-auto md:ml-[150px] ml-[70px]">
+          <h1 className="bg-[#EBEBEB] md:w-[300px] w-[112px] text-center font-bold py-[15px] rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px] ">
+            Exam Dates
+          </h1>
+          <p className="flex -ml-[13px] my-4">
+            <FaCircle className="my-auto mr-3 text-[22px] text-[#BE4E1E]" /> Part I - May 11 - 17, 2024
+          </p>
+          <p className="flex -ml-[13px] my-4">
+            <FaCircle className="my-auto mr-3 text-[22px] text-[#BE4E1E]" /> Part II - May 18 - 22, 2024
+          </p>
+        </div>
+      </div> */}
+      <div>
+      <div class="border-l-2 sm:border-l-4  max-w-[700px] mx-auto ml-[70px] md:ml-[220px] lg:ml-[449px]  m-9 ">
+        <h1 className="bg-[#EBEBEB]  w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
+          Exam Dates
+        </h1>
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Part I - May 11 - 17,2024
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+          Part II - May 18 - 22,2024
+        </p>
 
-           <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-bold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px] text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Registration
         </h1>
-      </div>   
 
-      <div class="border-l-4 border-gray-400  ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Early - Dec 1, 2023 - Jan 31, 2024</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Standard - Feb 1 - March 31, 2024</h1>
-        </div> 
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Early - Dec 1,2023 - Jan 31,2024
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+          Standard - Feb 1 - March 31,2024
+        </p>
 
-        <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px]  sm:w-[300px] py-[15px] rounded-full  text-[14px] sm:text-[24px] font-bold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Schedule
         </h1>
-      </div>   
 
-      <div class="border-l-4  border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px] ">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[8px] sm:-ml-[14px] pt-8 h-1"><FaCircle className="my-auto  mr-5 text-[#BE4E1E]"/><span className="font-semibold -mt-1">Window to Select an Exam - </span>&nbsp;<span className="-mt-1 ml-[35px] sm:ml-0">Dec 1, 2023 - April 26, 2024</span></h1>
-        </div> 
+        <div className="text-[14px] sm:text-[24px]  flex   -ml-2  sm:-ml-[14px] pt-8 h-1">
+          <FaCircle className="my-auto  mr-4 text-[#BE4E1E] flex-shrink-0" />
+          <ul className="list-none flex flex-wrap -mt-1 ">
+            <li className="pr-2  font-bold">Window to Select an Exam -</li>
+            <li>Dec 1,2023  -  April 26,2024</li>
+          </ul>
+        </div>
+      </div>
+      </div>
 
-        <div className="bg-[#FFEFCB] py-4 mt-[97px]">
-        <h1 className="font-bold mx-auto max-w-[1350px] text-center sm:text-left px-[25px] text-[14px] sm:text-[36px]">
+      <div className="bg-[#FFEFCB] py-4 mt-[97px]">
+        <h1 className="font-bold mx-auto max-w-[1350px] text-center  px-[25px] text-[14px] sm:text-[36px]">
           AUGUST EXAMS
         </h1>
       </div>
 
-      <div className="max-w-[1350px] mx-auto px-[25px] mt-[41px]">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+      <div>
+      <div class="border-l-2 sm:border-l-4  max-w-[700px] mx-auto ml-[70px] md:ml-[220px] lg:ml-[449px]  m-9 ">
+        <h1 className="bg-[#EBEBEB]  w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Exam Dates
         </h1>
-      </div>
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Part I - August 9 - 10,2024  AM Sessions
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+       Part II - August 9 - 10,2024  PM Sessions
+        </p>
 
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part I - August 9 - 10, 2024 AM Sessions</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part II - August 9  - 22, 2024 PM Sessions</h1>
-        </div>  
-
-        <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px] text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Registration
         </h1>
-      </div>   
 
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Early - March 1 - April 30, 2024</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Standard - May 1 - June 30, 2024</h1>
-        </div> 
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Early - March 1 - April 30,2024
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Standard - May 1 - June 30,2024
+        </p>
 
-        
-        <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Schedule
         </h1>
-      </div>   
 
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[8px] sm:-ml-[14px] pt-8 h-1"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/><span className="font-semibold -mt-1">Window to Select an Exam - </span>&nbsp;<span className="-mt-1">March 1 - July 26, 2024</span></h1>
-        </div> 
-
-        <div className="bg-[#FFEFCB] py-4 mt-[97px]">
-        <h1 className="font-bold mx-auto max-w-[1350px] text-center sm:text-left px-[25px] text-[14px] sm:text-[36px]">
+        <div className="text-[14px] sm:text-[24px]  flex   -ml-2  sm:-ml-[14px] pt-8 h-1">
+          <FaCircle className="my-auto  mr-4 text-[#BE4E1E] flex-shrink-0" />
+          <ul className="list-none flex flex-wrap -mt-1 ">
+            <li className="pr-2  font-bold">Window to Select an Exam -</li>
+            <li>March 1 -  July 26, 2024</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+      <div className="bg-[#FFEFCB] py-4 mt-[97px]">
+        <h1 className="font-bold mx-auto max-w-[1350px] text-center  px-[25px] text-[14px] sm:text-[36px]">
           NOVEMBER EXAMS
         </h1>
       </div>
 
-      <div className="max-w-[1350px] mx-auto px-[25px] mt-[41px]">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+      <div>
+      <div class="border-l-2 sm:border-l-4  max-w-[700px] mx-auto ml-[70px] md:ml-[220px] lg:ml-[449px]  m-9 ">
+        <h1 className="bg-[#EBEBEB]  w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Exam Dates
         </h1>
-      </div>
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Part I - Nov 9 - 15,2024
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Part II - Nov 16 - 19,2024
+        </p>
 
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part I - Nov 9 - 15, 2024</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Part II - Nov 16  - 19, 2024</h1>
-        </div>  
-
-        <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px] text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Registration
         </h1>
-      </div>   
 
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] py-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Early - May 1 - July 31, 2024</h1>
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[9px] sm:-ml-[14px] pb-4"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/>Standard - Aug 1 - Sept 30, 2024</h1>
-        </div> 
+        <p className="text-[14px] sm:text-[24px] flex -ml-2 sm:-ml-[14px] my-4">
+        <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+        Early - May 1 - July 31,2024
+        </p>
+        <p className="text-[14px] sm:text-[24px]  flex -ml-2 sm:-ml-[14px] my-4">
+          <FaCircle className="my-auto mr-3 text-[#BE4E1E]" />
+          Standard - Aug 1 - Sept 30,2024
+        </p>
 
-        <div className="max-w-[1350px] mx-auto px-[25px] ">
-        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[300px] py-[15px] rounded-full text-[14px] sm:text-[24px] font-semibold text-center">
+        <h1 className="bg-[#EBEBEB] w-[112px] sm:w-[226px] md:w-[300px]  text-center font-bold py-3 rounded-full text-[14px] sm:text-[24px] md:-ml-[150px] -ml-[60px]">
           Schedule
         </h1>
-      </div>   
 
-       
-      <div class="border-l-4 border-gray-400 ml-[79px] md:ml-[169px] lg:ml-[459px]">
-          <h1 className="text-[14px] sm:text-[24px]  flex -ml-[8px] sm:-ml-[14px] pt-4 h-1"><FaCircle className="my-auto mr-5 text-[#BE4E1E]"/><span className="font-semibold -mt-1">Window to Select an Exam - </span>&nbsp;<span className="-mt-1">May 1 - Oct 25, 2024</span></h1>
-        </div> 
-
-   
+        <div className="text-[14px] sm:text-[24px]  flex   -ml-2  sm:-ml-[14px] pt-8 h-1">
+          <FaCircle className="my-auto  mr-4 text-[#BE4E1E] flex-shrink-0" />
+          <ul className="list-none flex flex-wrap -mt-1 ">
+            <li className="pr-2  font-bold">Window to Select an Exam -</li>
+            <li>May 1 -  Oct 25,2024</li>
+          </ul>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
