336e60bbc521458eb94e7f98513d4ee90788c373
Sujal Sahu <sahusujal620@gmail.com>
2024-05-02 13:53:16 +0530

    done

--- app/[pageSlug]/[page]/page.js
+++ app/[pageSlug]/[page]/page.js
@@ -7,13 +7,18 @@ const RedirectPage = () => {
     const [timer, setTimer] = useState(3);
 
     useEffect(() => {
+        const redirectTimer = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+
         // Redirect to the home page after 3 seconds
         const redirectTimeout = setTimeout(() => {
             router.push('/');
-        }, 100);
+        }, 3000);
 
         // Clear the timer and timeout when the component is unmounted
         return () => {
+            clearInterval(redirectTimer);
             clearTimeout(redirectTimeout);
         };
     }, [router]);
@@ -21,9 +26,9 @@ const RedirectPage = () => {
     return (
         <div className='w-[100%]'>
             <div>
-                <Image src='/404.jpg' width={600 } height={600} alt='404 Error' className='mx-auto w-[300px] md:w-[600px] '/>
-                <div className="text-center text-[1.2rem] md:text-[1.8rem] my-2 font-semibold p-2 tracking-wide">
-                    404 Page Not Found, Redirecting To Homepage  ...
+                <Image src='/404.jpg' width={600} height={600} alt='404 Error' className='mx-auto w-[300px] md:w-[600px] ' priority={true} />
+                <div className="text-center text-[1.2rem] my-2 font-semibold p-2 tracking-wide">
+                    404 Page Not Found , Redirecting To Homepage in {timer} seconds ...
                 </div>
             </div>
         </div>
