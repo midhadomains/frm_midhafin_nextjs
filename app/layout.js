2e6c48d0dabcd4458be7b076c0c07854f1ee233d
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-05-27 17:39:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,11 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Boost Your Career In Finance" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.frm.midhafin.com/" />
-        <meta property="og:image" content="https://ogcdn.net/c078a98b-9a33-4eaf-a5cf-e5ebf3ea450c/v1/frm.midhafin.com/MidhaFin/Boost%20Your%20Career%20In%20Finance/https%3A%2F%2Fcdn.theopengraph.com%2Fproduction%2Fdocuments%2Fd564a463-1c51-4259-991d-0d17bf3e0391.jpg%3Ftoken%3D-hKrjcO4EFDmPh0lgxSanJdH2MZ9gYnfw4V87WUckbA%26height%3D800%26width%3D1200%26expires%3D33239188519/og.png" />
+        <meta property="og:image" content="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Homepage/open-graph-snippet_600*336.png.png" />
         <meta name="x:card" content="summary_large_image" />
         <meta name="x:title" content="MidhaFin:- Best prep provider" />
         <meta name="x:description" content="Boost Your Career In Finance" />
-        <meta name="x:image" content="https://ogcdn.net/c078a98b-9a33-4eaf-a5cf-e5ebf3ea450c/v1/frm.midhafin.com/MidhaFin/Boost%20Your%20Career%20In%20Finance/https%3A%2F%2Fcdn.theopengraph.com%2Fproduction%2Fdocuments%2Fd564a463-1c51-4259-991d-0d17bf3e0391.jpg%3Ftoken%3D-hKrjcO4EFDmPh0lgxSanJdH2MZ9gYnfw4V87WUckbA%26height%3D800%26width%3D1200%26expires%3D33239188519/og.png" />
+        <meta name="x:image" content="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Homepage/open-graph-snippet_600*336.png.png" />
       </head>
       <body className={inter.className}>
         <Whatsapp />
